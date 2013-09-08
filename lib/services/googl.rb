6392c9e60d6541f4971f2d9bc08cbe068a0a64cd
Lucas Allan Amorim <lucas.allan@gmail.com>
2013-09-08 15:36:03 -0300

    small refactory

--- lib/services/googl.rb
+++ lib/services/googl.rb
@@ -5,21 +5,28 @@ module Googl
   include HTTParty
 
   API_URL = 'https://www.googleapis.com/urlshortener/v1/url'
-  
+
   def self.encode(url)
-    change_header 
-    options = {"longUrl" => url}.inspect
-    resp = self.post(API_URL, :body => options)
+    options = {"shortUrl" => url}.inspect
+    format_response(post_request(url, options))
+  end
+
+  private
+
+  def self.add_header
+    self.headers.merge!('Content-Type' => 'application/json')
+  end
+
+  def self.post_request(url, options)
+    add_header
+    self.post(API_URL, body: options)
+  end
+
+  def self.format_response(resp)
     if resp.code == 200
       resp['id']
     else
       resp.response
     end
-  end
-
-  protected
-
-  def self.change_header
-    self.headers.merge!('Content-Type' => 'application/json')
   end
 end
--- spec/harakiri_spec.rb
+++ spec/harakiri_spec.rb
@@ -1,12 +1,13 @@
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe Harakiri do
-  it "should shorten with tinyurl" do
+
+  it "shortens url using tinyurl" do
     Harakiri.tiny_encode("www.lucasallan.com").should == "http://tinyurl.com/26pwxbx"
   end
 
-  it "should shorten with googl" do
-   Harakiri.googl_encode("www.lucasallan.com").should == "http://goo.gl/c7R93" 
+  it "shortens with googl" do
+   Harakiri.googl_encode("www.lucasallan.com").should == "http://goo.gl/c7R93"
   end
 end
 
